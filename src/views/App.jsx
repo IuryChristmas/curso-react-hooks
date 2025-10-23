dc7fcd12133e4e28a2a898bdcf61545427fbfe68
Iury Christmas <iury.christmas@gmail.com>
2025-10-23 17:58:52 -0300

    criando projeto

---
+++ src/views/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter} from 'react-router-dom'
+
+import Menu from '../components/layout/Menu'
+import Content from '../components/layout/Content'
+
+const App = () => {
+
+    return (
+                <div className="App">
+                    <BrowserRouter>
+                        <Menu />
+                        <Content />
+                    </BrowserRouter>
+                </div>
+    )
+}
+
+export default App
