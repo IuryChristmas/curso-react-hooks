97b7e47add45f3a67e7ef19bc6534b879823067e
Iury Christmas <iury.christmas@gmail.com>
2025-11-25 19:51:00 -0300

    concluida aula numero 1 referente ao useContext

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -4,15 +4,21 @@ import { BrowserRouter} from 'react-router-dom'
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
+import { DataContext, data } from '../data/DataContext'
+import { useState } from 'react'
+
 const App = () => {
+    const [state, setState] = useState(data)
 
     return (
+        <DataContext.Provider value={{state, setState}}>
                 <div className="App">
                     <BrowserRouter>
                         <Menu />
                         <Content />
                     </BrowserRouter>
                 </div>
+        </DataContext.Provider>
     )
 }
 
